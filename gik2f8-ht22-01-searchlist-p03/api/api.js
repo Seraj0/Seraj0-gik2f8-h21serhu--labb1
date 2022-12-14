da386bc622fea455da9cd8bf1cc952fd9b2f9240
Seraj husseino <h21serhu@du.se>
2022-12-14 14:42:00 +0100

    Lagt till kommentar

--- gik2f8-ht22-01-searchlist-p03/api/api.js
+++ gik2f8-ht22-01-searchlist-p03/api/api.js
@@ -1,8 +1,8 @@
 const url = 'https://gik2f8-labs.herokuapp.com/books';
 
 async function getAll() {
-  const result = await fetch(url)
-    .then((result) => result.json())
+  const result = await fetch(url) 
+    .then((result) => result.json())   //Innehåller array med lista av böckerna från URL
     .catch((e) => e);
 
   return result;
